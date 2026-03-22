a6efb8c407ddd000d352c39e412fa778a375143d
Gleb Neverov <hellhands@yandex.ru>
2026-03-22 17:35:43 +0700

    - какие то правки

---
+++ local/src/config/index.js
@@ -0,0 +1,6 @@
+export const contacts = {
+    phone1: '8 800 <b>302-09-58</b>',
+    phone2: '+7 495 <b>409-10-58</b>',
+    email: 'info@hermle-store.ru',
+    address: 'Москва, ул. Шаболовка д.23 кор.5 (1 этаж)',
+};
