6b7dfaaf934a5f3efc39f01c77478c53b0af1089
Gleb Neverov <hellhands@yandex.ru>
2026-04-02 09:28:13 +0700

    Feat:(Landing) добавлен блок "Отзывы"

--- local/src/config/index.js
+++ local/src/config/index.js
@@ -1,6 +1,6 @@
 export const contacts = {
-    phone1: '8 800 <b>302-09-58</b>',
-    phone2: '+7 495 <b>409-10-58</b>',
-    email: 'info@hermle-store.ru',
+    phone: '+7 (993) 206-63-68',
+    phoneHref: '+79932066368',
+    email: 'podoexpert@mail.ru',
     address: 'Москва, ул. Шаболовка д.23 кор.5 (1 этаж)',
 };
