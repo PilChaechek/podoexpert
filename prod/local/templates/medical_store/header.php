b58177dc765c6ed67bd24cafb54f93e932c329d4
Gleb Neverov <hellhands@yandex.ru>
2026-04-30 17:11:10 +0700

    Feat(Prod) - Базовый catalog_search

--- prod/local/templates/medical_store/header.php
+++ prod/local/templates/medical_store/header.php
@@ -118,7 +118,83 @@ $cartCountBadge = function_exists('podexpert_basket_count_badge_text')
 
 
 
+
                 <div class="page-header__search">
+
+
+                    <?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.search", 
+	".default", 
+	[
+		"AJAX_MODE" => "Y",
+		"IBLOCK_TYPE" => "catalog",
+		"IBLOCK_ID" => "11",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"ELEMENT_SORT_FIELD2" => "id",
+		"ELEMENT_SORT_ORDER2" => "desc",
+		"SECTION_URL" => "",
+		"DETAIL_URL" => "",
+		"BASKET_URL" => "/personal/basket.php",
+		"ACTION_VARIABLE" => "action",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"SECTION_ID_VARIABLE" => "SECTION_ID",
+		"DISPLAY_COMPARE" => "Y",
+		"PAGE_ELEMENT_COUNT" => "30",
+		"LINE_ELEMENT_COUNT" => "3",
+		"PROPERTY_CODE" => [
+			0 => "",
+			1 => "",
+		],
+		"OFFERS_FIELD_CODE" => "",
+		"OFFERS_PROPERTY_CODE" => "",
+		"OFFERS_SORT_FIELD" => "sort",
+		"OFFERS_SORT_ORDER" => "asc",
+		"OFFERS_SORT_FIELD2" => "id",
+		"OFFERS_SORT_ORDER2" => "desc",
+		"OFFERS_LIMIT" => "5",
+		"PRICE_CODE" => [
+			0 => "base",
+		],
+		"USE_PRICE_COUNT" => "Y",
+		"SHOW_PRICE_COUNT" => "1",
+		"PRICE_VAT_INCLUDE" => "Y",
+		"USE_PRODUCT_QUANTITY" => "Y",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"RESTART" => "Y",
+		"NO_WORD_LOGIC" => "Y",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"CHECK_DATES" => "Y",
+		"DISPLAY_TOP_PAGER" => "Y",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Товары",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_DESC_NUMBERING" => "Y",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "Y",
+		"HIDE_NOT_AVAILABLE" => "N",
+		"CONVERT_CURRENCY" => "Y",
+		"CURRENCY_ID" => "RUB",
+		"OFFERS_CART_PROPERTIES" => "",
+		"AJAX_OPTION_JUMP" => "Y",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "Y",
+		"COMPONENT_TEMPLATE" => ".default",
+		"HIDE_NOT_AVAILABLE_OFFERS" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"PRODUCT_PROPERTIES" => [
+		],
+		"USE_TITLE_RANK" => "N",
+		"USE_SEARCH_RESULT_ORDER" => "N"
+	],
+	false
+);?>
+
+
                     <form class="page-header__search-form flex w-full items-center relative" action="/search/" method="get" role="search">
                         <label class="hidden" for="header-search-q">Поиск по сайту</label>
                         <input id="header-search-q" class="input" type="search" name="q" placeholder="Поиск..." autocomplete="off">
